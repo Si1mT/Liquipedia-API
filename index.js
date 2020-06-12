065845460cc48b43b08107ef350824117921f97e
Si1mT <siim.tanavots@tptlive.ee>
2020-06-12 14:53:05 +0300

    added /majorevents endpoint

--- index.js
+++ index.js
@@ -12,13 +12,30 @@ app.get("/all", (req, res, next) => {
         
         $('a', '.wiki-header', html).each(function(i, elem){
             games[i] = $(elem).text();
-        })
+        });
         
         res.json(games);
     })
     .catch(function(err){
   });
 });
+
+app.get("/majorevents", (req, res, next) => {
+    rp(url + 'counterstrike/Main_Page')
+    .then(function(html){
+        
+        var majorEvents = [];
+        
+        $('.panel-box-heading.wiki-color-dark.wiki-backgroundcolor-light.wiki-bordercolor-light', '.lp-col.lp-order-2.lp-col-12', html).each(function(i, elem){
+            majorEvents[i] = $(elem).text().trim();
+        });
+
+        res.json(majorEvents);
+    })
+    .catch(function(err){
+  });
+})
+
 
 app.listen(port, () => {
  console.log("Server running on port " + port);
